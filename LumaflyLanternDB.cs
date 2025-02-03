461fe9f95cf2947da6cde7981237a3e582fca118
czlabinger <czlabinger@student.tgm.ac.at>
2025-02-03 14:06:12 +0100

    Attaches tracker to lanterns in DB, collision working needs check for nail

--- LumaflyLanternDB.cs
+++ LumaflyLanternDB.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace LumaflyLanternTracker {
     internal class LumaflyLanternDB {
@@ -7,12 +8,11 @@ namespace LumaflyLanternTracker {
         //Serialized LanternKey, LanternState
         internal static Dictionary<string[], LanternState> list = new Dictionary<string[], LanternState>
         {
-            {[ "UgB1AGkAbgBzADEAXwAyADkA", "cgB1AGkAbgBkAF8AZAByAGUAcwBzAGkAbgBnAF8AbABpAGcAaAB0AF8AMAAzACAAKAAxACkA", "QVCj1w==", "QS3rhQ==" ], LanternState.DEFAULT },
-            {[ "UgB1AGkAbgBzADEAXwAyADkA", "cgB1AGkAbgBkAF8AZAByAGUAcwBzAGkAbgBnAF8AbABpAGcAaAB0AF8AMAAzACAAKAAyACkA", "QZnXCg==", "QTqPXA==" ], LanternState.DEFAULT },
-            {[ "UgB1AGkAbgBzADEAXwAyADkA", "cgB1AGkAbgBkAF8AZAByAGUAcwBzAGkAbgBnAF8AbABpAGcAaAB0AF8AMAAzACAAKAA0ACkA", "QMlHrg==", "QTVwpA==" ], LanternState.DEFAULT },
-            {[ "UgB1AGkAbgBzADEAXwAyADkA", "cgB1AGkAbgBkAF8AZAByAGUAcwBzAGkAbgBnAF8AbABpAGcAaAB0AF8AMAAzACAAKAA1ACkA", "QYceuA==", "QTrhSA==" ], LanternState.DEFAULT },
-            {[ "UgB1AGkAbgBzADEAXwAyADkA", "cgB1AGkAbgBkAF8AZAByAGUAcwBzAGkAbgBnAF8AbABpAGcAaAB0AF8AMAAzACAAKAA3ACkA", "QRqPXA==", "QTQo9g==" ], LanternState.DEFAULT },
-
+            {new LanternKey("Ruins1_29", "ruind_dressing_light_03 (1)", new Vector2(13.04f, 10.87f)).Serialize(), LanternState.DEFAULT },
+            {new LanternKey("Ruins1_29", "ruind_dressing_light_03 (2)", new Vector2(19.23f, 11.66f)).Serialize(), LanternState.DEFAULT },
+            {new LanternKey("Ruins1_29", "ruind_dressing_light_03 (4)", new Vector2(6.29f, 11.34f)).Serialize(), LanternState.DEFAULT },
+            {new LanternKey("Ruins1_29", "ruind_dressing_light_03 (5)", new Vector2(16.89f, 11.68f)).Serialize(), LanternState.DEFAULT },
+            {new LanternKey("Ruins1_29", "ruind_dressing_light_03 (7)", new Vector2(9.66f, 11.26f)).Serialize(), LanternState.DEFAULT },
         };
 
     }
